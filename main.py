33c64b0995cc6935c8da1f29779c1f82eba5ab19
Aaron Graddy Jr <atgraddyjr@gmail.com>
2023-01-12 22:06:11 -0600

    bad attempt at doing some machine learning

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import statistics
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, multipliers, random_card_choosing=False):
         self.cards = []
+        self.multipliers = multipliers
+        self.random_choosing = random_card_choosing
 
 
 class Card:
@@ -71,8 +73,7 @@ def find_playable_cards(current_player=None, cc=None, single_card=None):
         return len(playable_cards) == 0
 
 
-def play_one_game(no_players):
-    players = [Player() for i in range(no_players)]
+def play_one_game(no_players, players, random_ai):
     create_deck()
     # Dealing out cards
     for _ in range(7):  # Number of cards
@@ -87,46 +88,86 @@ def play_one_game(no_players):
     del deck[0]
     stop = False
     direction = "right"
+    if len(deck) == 0:
+        create_deck()
     while cc.special_description:
         cc = deck[0]
         del deck[0]
+        if len(deck) == 0:
+            create_deck()
     while not stop:
         current_player = players[current_player_index]  # Setting current player
         playable_cards = find_playable_cards(current_player, cc)
         # Defining the value of each playable card (how good it is)
         if len(playable_cards) > 0:  # Can I play a card?
-            playable_card_values = []  # Keeps track of how good each card is
-            # 0 number value = -100 (GET RID OF IT!)
-            # +2 cards = 0.1 (just barely better than a zero card
-            # Random card (just a color and number) = 1 (Value will decrease as we have more of that color in hand)
-            # Special not wild or +4 or +2 = 5
-            # Wild or +4 = 6
-            for c in playable_cards:
-                if c.number == 0:  # 0 number
-                    playable_card_values.append(-100)
-                    continue
-                if not c.special_description:  # Random card
-                    value = 1
-                    for k in current_player.cards:
-                        if k.color == c.color:
-                            value *= 1.1
-                    value -= 1
-                    playable_card_values.append(value)
-                    continue
-                if c.special_description:
-                    if c.special_description != "wild" and c.special_description != "+4" and c.special_description != "+2":
-                        playable_card_values.append(5)
+            if current_player.random_choosing:
+                playable_card_values = []  # Keeps track of how good each card is
+                # V2 COST MULTIPLIERS:
+                # most_color = FIND HOW MANY OF THAT COLOR APPEAR IN HAND
+                # Random Card: (ratio_in_deck + most_color) * MULTIPLIER
+                # Special Card: (ratio_in_deck + next_player_amount_cards + most_color) * MULTIPLIER
+                # Wild: DON'T PLAY IT
+                # +4: (ratio_in_deck + next_player_amount_cards) * MULTIPLIER
+                for c in playable_cards:
+
+                    if not c.special_description:  # Random card
+                        ratio_in_deck = 0  # work in progress
+                        most_color = [c.color for c in current_player.cards].count(statistics.mode([card.color for card in current_player.cards]))
+                        playable_card_values.append((ratio_in_deck + most_color) * current_player.multipliers[0])
                         continue
+
+                    elif c.special_description == "wild":
+                        playable_card_values.append(current_player.multipliers[1])
+
+                    elif c.special_description == "reverse":
+                        temp_current_player_index = current_player_index
+                        if direction == "right":
+                            temp_current_player_index += 1
+                            if temp_current_player_index > no_players - 1:
+                                temp_current_player_index = 0
+                        else:
+                            temp_current_player_index -= 1
+                            if temp_current_player_index < 0:
+                                temp_current_player_index = no_players - 1
+                        next_player_cards = len(players[temp_current_player_index].cards)
+                        playable_card_values.append(next_player_cards * current_player.multipliers[2])
+
+                    elif c.special_description == "skip":
+                        temp_current_player_index = current_player_index
+                        if direction == "right":
+                            temp_current_player_index += 1
+                            if temp_current_player_index > no_players - 1:
+                                temp_current_player_index = 0
+                        else:
+                            temp_current_player_index -= 1
+                            if temp_current_player_index < 0:
+                                temp_current_player_index = no_players - 1
+                        next_player_cards = len(players[temp_current_player_index].cards)
+                        playable_card_values.append(next_player_cards * current_player.multipliers[3])
+
                     elif c.special_description == "+2":
-                        playable_card_values.append(0.1)
-                        continue
-                    else:
-                        playable_card_values.append(6)
-                        continue
-            cc = playable_cards[playable_card_values.index(min(playable_card_values))]  # Playing the card
+                        temp_current_player_index = current_player_index
+                        if direction == "right":
+                            temp_current_player_index += 1
+                            if temp_current_player_index > no_players - 1:
+                                temp_current_player_index = 0
+                        else:
+                            temp_current_player_index -= 1
+                            if temp_current_player_index < 0:
+                                temp_current_player_index = no_players - 1
+                        next_player_cards = len(players[temp_current_player_index].cards)
+                        playable_card_values.append(next_player_cards * current_player.multipliers[4])
+
+                    elif c.special_description == "+4":
+                        playable_card_values.append(current_player.multipliers[5])
+
+                cc = playable_cards[playable_card_values.index(min(playable_card_values))]  # Playing the card
+            else:
+                cc = playable_cards[random.randint(0, len(playable_cards)-1)]  # Playing the card
             del current_player.cards[current_player.cards.index(cc)]
             if len(current_player.cards) == 0:  # If game is over:
                 break
+
             # Playing special card
             if cc.special_description == "skip":  # Skip
                 if direction == "right":
@@ -181,6 +222,8 @@ def play_one_game(no_players):
                     temp_current_player_index -= 1
                     if temp_current_player_index < 0:
                         temp_current_player_index = no_players - 1
+                if len(deck) == 0:
+                    create_deck()
                 for i in range(2):
                     players[temp_current_player_index].cards.append(deck[0])
                     del deck[0]
@@ -195,8 +238,11 @@ def play_one_game(no_players):
                     current_player_index -= 1
                     if current_player_index < 0:
                         current_player_index = no_players - 1
+
         else:  # You don't have a card to play
             no_times_drew = 1
+            if len(deck) == 0:
+                create_deck()
             while find_playable_cards(cc=cc, single_card=deck[0]):  # While you can't play the card you drew
                 no_times_drew += 1
                 current_player.cards.append(deck[0])
@@ -207,6 +253,7 @@ def play_one_game(no_players):
             del deck[0]
             if len(deck) == 0:
                 create_deck()
+
         no_turns += 1
         # Changing player index
         if direction == "right":
@@ -217,17 +264,51 @@ def play_one_game(no_players):
             current_player_index -= 1
             if current_player_index < 0:
                 current_player_index = no_players-1
-    return no_turns
+
+    for i in players:
+        if i.random_choosing:
+            if players[current_player_index].random_choosing:
+                return "Random Won!"
+            else:
+                return "AI Won!"
+    return no_turns, players[current_player_index].multipliers
 
 
-def simulate(no_players, no_games):
+def simulate(no_players, no_games, random_choosing):
     all_games = []
     for game in range(no_games):
-        all_games.append(play_one_game(no_players))
-    return "Average Number Of Turns: {}".format(round(statistics.mean(all_games)))
+        if not random_choosing:
+            players = [Player(multipliers=[1.750000000000001, -0.4500000000000003, -2.589999999999989, -3.48999999999997, -0.5500000000000005, -0.9100000000000009]) for i in range(no_players)]
+        else:
+            players = [Player(multipliers=[1.750000000000001, -0.4500000000000003, -2.589999999999989, -3.48999999999997, -0.5500000000000005, -0.9100000000000009]) for i in range(no_players-1)]
+            players.append(Player(multipliers=[0, 0, 0, 0, 0, 0], random_card_choosing=True))
+        all_games.append(play_one_game(no_players, players, False)[0])
+        print(play_one_game(no_players, players, False))
 
 
-print(simulate(4, 1000))
+def find_multipliers(no_players, generations):
+    random_card = 0
+    wild = 0
+    reverse = 0
+    skip = 0
+    plus2 = 0
+    plus4 = 0
+    game_winning_multipliers = None
+    for generation in range(generations):
+        players = [Player(multipliers=[random_card, wild, reverse, skip, plus2, plus4]) for i in range(no_players)]
+        game_winning_multipliers = play_one_game(no_players, players, False)[1]
+        random_card = game_winning_multipliers[0] + [-0.01, 0.01][random.randint(0, 1)]
+        wild = game_winning_multipliers[1] + [-0.01, 0.01][random.randint(0, 1)]
+        reverse = game_winning_multipliers[2] + [-0.01, 0.01][random.randint(0, 1)]
+        skip = game_winning_multipliers[3] + [-0.01, 0.01][random.randint(0, 1)]
+        plus2 = game_winning_multipliers[4] + [-0.01, 0.01][random.randint(0, 1)]
+        plus4 = game_winning_multipliers[5] + [-0.01, 0.01][random.randint(0, 1)]
+        print("Generation: {}  {}%".format(generation+1, generation/generations * 100))
+    return game_winning_multipliers
+
+
+print(simulate(2, 1, True))
+# print(find_multipliers(10000, 100000))
 
 # This project follows the official UNO game rules (Which means no stacking +2/+4s), and is optimised to play the
 # theoretical best move.
